62a940fc44e383c6c4ebf28c57d91c197ea6d9ce
Alejandro Roble Gutierrez <arobleg@gmail.com>
2023-08-17 16:58:52 -0400

    Implement test to validate merge array schema

--- tests/merge_array_test.py
+++ tests/merge_array_test.py
@@ -1,6 +1,11 @@
 import json
+import jsonschema
+import pytest
 
 from api.services.algorithms import merge_array
+from api.schemas.merge_array_schema import merge_array_schema
+from hypothesis import given
+from hypothesis.strategies import lists, integers, text, booleans, floats
 from tests.conftest import helper
 
 ordered_1 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
@@ -47,3 +52,50 @@ def test_merge_array_endpoint_with_unordered_list(client):
     assert response_info["success"] is True
     assert response_info["mixed_ordered_list"] == [1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
                                                    13, 14, 15, 16, 17, 18, 19, 20]
+
+
+@given(lists(elements=integers(min_value=1, max_value=100), min_size=1, max_size=20),
+       lists(elements=integers(min_value=1, max_value=100), min_size=1, max_size=20))
+def test_merge_array_schema_good_way(list_1: list, list_2: list) -> None:
+
+    td_post_body = {
+        "list_1": list_1,
+        "list_2": list_2,
+    }
+
+    assert isinstance(list_1, list)
+    assert all(isinstance(element, int) for element in list_1)
+    assert isinstance(list_2, list)
+    assert all(isinstance(element, int) for element in list_2)
+    jsonschema.validate(td_post_body, merge_array_schema)
+
+
+@pytest.mark.xfail
+@given(list_1=lists(floats(), min_size=1, max_size=20),
+       list_2=lists(text(), min_size=1, max_size=20),
+       list_3=lists(booleans(), min_size=1, max_size=20),
+       list_4=lists(integers(), min_size=1, max_size=20))
+def test_merge_array_schema_mixed_list(list_1: list, list_2: list,
+                                       list_3: list, list_4: list) -> None:
+
+    td_post_body_variant_1 = {
+        "list_1": list_1,
+        "list_2": list_2,
+    }
+
+    td_post_body_variant_2 = {
+        "list_1": list_1,
+        "list_2": list_3,
+    }
+
+    td_post_body_variant_3 = {
+        "list_1": list_1,
+        "list_2": list_4,
+    }
+    assert isinstance(list_1, list)
+    assert isinstance(list_2, list)
+    assert isinstance(list_3, list)
+    assert isinstance(list_4, list)
+    jsonschema.validate(td_post_body_variant_1, merge_array_schema)
+    jsonschema.validate(td_post_body_variant_2, merge_array_schema)
+    jsonschema.validate(td_post_body_variant_3, merge_array_schema)
