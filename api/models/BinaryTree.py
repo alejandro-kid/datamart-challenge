03a55e5caf3b2b0d27fa98f61d01920094578cbd
Alejandro Roble Gutierrez <arobleg@gmail.com>
2023-08-18 04:53:42 -0400

    Implement BinaryTree and Node classes

---
+++ api/models/BinaryTree.py
@@ -0,0 +1,41 @@
+from api.models.Node import Node
+
+
+class BinaryTree(object):
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        if self.root:
+            return self.root.insert(data)
+        else:
+            self.root = Node(data)
+            return True
+
+    def delete(self, data):
+        if self.root is not None:
+            return self.root.delete(data,self.root)
+
+    def find(self, data):
+        if self.root:
+            return self.root.find(data)
+        else:
+            return False
+
+    def preorder(self):
+        if self.root is not None:
+            print()
+            print('Preorder: ')
+            self.root.preorder()
+
+    def inorder(self):
+        print()
+        if self.root is not None:
+            print('Inorder: ')
+            self.root.inorder()
+
+    def postorder(self):
+        print()
+        if self.root is not None:
+            print('Postorder: ')
+            self.root.postorder()
---
+++ api/models/Node.py
@@ -0,0 +1,101 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.leftChild = None
+        self.rightChild = None
+
+    def insert(self, data):
+        if self.data == data:
+            return False 
+
+        elif data < self.data:
+            if self.leftChild:
+                return self.leftChild.insert(data)
+            else:
+                self.leftChild = Node(data)
+                return True
+
+        else:
+            if self.rightChild:
+                return self.rightChild.insert(data)
+            else:
+                self.rightChild = Node(data)
+                return True
+
+    def minValueNode(self, node):
+        current = node
+
+        while(current.leftChild is not None):
+            current = current.leftChild
+
+        return current
+
+    def maxValueNode(self, node):
+        current = node
+
+        while(current.rightChild is not None):
+            current = current.rightChild
+
+        return current
+
+
+    def delete(self, data,root):
+        if self is None:
+            return None
+
+        if data < self.data:
+            self.leftChild = self.leftChild.delete(data,root)
+        elif data > self.data:
+            self.rightChild = self.rightChild.delete(data,root)
+        else:
+            # deleting node with one child
+            if self.leftChild is None:
+
+                if self == root:
+                    temp = self.minValueNode(self.rightChild)
+                    self.data = temp.data
+                    self.rightChild = self.rightChild.delete(temp.data,root) 
+
+                temp = self.rightChild
+                self = None
+                return temp
+            elif self.rightChild is None:
+
+                if self == root:
+                    temp = self.maxValueNode(self.leftChild)
+                    self.data = temp.data
+                    self.leftChild = self.leftChild.delete(temp.data,root) 
+
+                temp = self.leftChild
+                self = None
+                return temp
+
+            # deleting node with two children
+            # first get the inorder successor
+            temp = self.minValueNode(self.rightChild)
+            self.data = temp.data
+            self.rightChild = self.rightChild.delete(temp.data,root)
+
+        return self
+
+    def find(self, data):
+        found = False
+
+        if(data == self.data):
+            found = True
+        elif(data < self.data):
+            if self.leftChild:
+                found = self.leftChild.find(data)
+        else:
+            if self.rightChild:
+                found = self.rightChild.find(data)
+        
+        return found
+
+    def inorder(self):
+        if self:
+            if self.leftChild:
+                self.leftChild.inorder()
+            print(str(self.data), end = ' ')
+            if self.rightChild:
+                self.rightChild.inorder()
