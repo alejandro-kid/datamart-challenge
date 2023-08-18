b5c43232e87a0b22df0b6996a1e42f084812d276
Alejandro Roble Gutierrez <arobleg@gmail.com>
2023-08-18 01:20:21 -0400

    Define validation schema for median endpoint

---
+++ api/schemas/median_schema.py
@@ -0,0 +1,10 @@
+median_schema = {
+    "type": "object",
+    "properties": {
+        "list": {
+            "type": "array",
+            "items": {"type": "number"}
+        }
+    },
+    "requiered": ["list"]
+}
